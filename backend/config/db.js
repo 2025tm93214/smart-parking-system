502456dc66a935850a0bf6d9a6f9f8dffaf269df
2025tm93214 <2025tm93214@wilp.bits-pilani.ac.in>
2026-04-20 12:18:53 +0530

    Update db.js
    Mongo DB URL removed for security purpose

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb+srv://2025tm93214_db_user:5dQ5Pcwv2KmpST8I@cluster0.dnqmthc.mongodb.net/parkingDB");
+    await mongoose.connect("Paste your mongo DB URL here");
     console.log("MongoDB Connected ✅");
   } catch (error) {
     console.error("DB Error:", error);
